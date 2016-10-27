73b20d45edc8c11887cd0d6cf456895eeb3991d5
Shahar Or <mightyiampresence@gmail.com>
2016-10-28 02:22:34 +0300

    Object.freeze the constants (fixes #55)

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,17 @@
-const actionNames = {
-  leftHide: 'LEFT_HIDE',
-  rightHide: 'RIGHT_HIDE',
-  leftUnhide: 'LEFT_UNHIDE',
-  rightUnhide: 'RIGHT_UNHIDE',
-  leftShoot: 'LEFT_SHOOT',
-  rightShoot: 'RIGHT_SHOOT'
+const constants = {
+  actionNames: {
+    leftHide: 'LEFT_HIDE',
+    rightHide: 'RIGHT_HIDE',
+    leftUnhide: 'LEFT_UNHIDE',
+    rightUnhide: 'RIGHT_UNHIDE',
+    leftShoot: 'LEFT_SHOOT',
+    rightShoot: 'RIGHT_SHOOT'
+  },
+  players: {
+    leftPlayer: 'LEFT_PLAYER',
+    rightPlayer: 'RIGHT_PLAYER'
+  }
 }
-module.exports.actionNames = actionNames
 
-const players = {
-  leftPlayer: 'LEFT_PLAYER',
-  rightPlayer: 'RIGHT_PLAYER'
-}
-module.exports.players = players
+Object.freeze(constants)
+module.exports = constants
