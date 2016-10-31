605ffa6100dee23148114f520a68b9d23777220b
Shahar <mightyiampresence@gmail.com>
2016-10-31 10:14:21 +0200

    arena test: set playerStub arity

--- src/ui-from-state/arena.test.js
+++ src/ui-from-state/arena.test.js
@@ -5,7 +5,7 @@ const { spy } = require('simple-spy')
 const R = require('ramda')
 
 const playerStubReturn = Symbol('playerStub')
-const playerStub = () => playerStubReturn
+const playerStub = (side, hiding) => playerStubReturn
 const playerSpy = spy(playerStub)
 test.afterEach(() => playerSpy.reset())
 mock('./player', R.curryN(2, playerSpy))
