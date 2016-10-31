6e0e44f411b263d3119edbfefc94d7c623eb0771
Shahar Or <mightyiampresence@gmail.com>
2016-11-01 00:43:38 +0200

    Use images for player character

--- src/ui-from-state/player.test.js
+++ src/ui-from-state/player.test.js
@@ -1,6 +1,10 @@
 const player = require('./player')
+const path = require('path')
+const urify = require('urify')
+const standingUri = urify(path.join(__dirname, 'player-not-hiding.png'))
+const hidingUri = urify(path.join(__dirname, 'player-hiding.png'))
 const { test } = require('ava')
-const { span } = require('@cycle/dom')
+const { img } = require('@cycle/dom')
 
 const possibleCallArgs = [
   ['left', false],
@@ -11,12 +15,19 @@ const possibleCallArgs = [
 
 const testWithCallArgs = ([side, hiding]) => {
   const testName = `vtree when side: ${side}, hiding: ${hiding}`
-  const expected = span(
-    {style: {
-      display: 'inline-block',
-      transform: side === 'left' ? null : 'scale(-1,1)'
-    }},
-    hiding ? 'd' : 'D'
+  const expected = img(
+    {
+      attrs: {
+        alt: hiding ? 'd' : 'D',
+        src: hiding ? hidingUri : standingUri
+      },
+      style: {
+        position: 'absolute',
+        bottom: 0,
+        [side]: 0,
+        transform: side === 'right' ? 'scale(-1,1)' : null
+      }
+    }
   )
   test(testName, t => {
     t.deepEqual(player(side, hiding), expected)
