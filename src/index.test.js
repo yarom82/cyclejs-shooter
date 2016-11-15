35c0cfd6ac279c09bd518f0872d58809d8afc37e
Shahar Or <mightyiampresence@gmail.com>
2016-11-15 23:49:18 +0200

    src/index.test.js: DOM -> vtree$

--- src/index.test.js
+++ src/index.test.js
@@ -6,8 +6,8 @@ const xstreamAdapter = require('@cycle/xstream-adapter').default
 test('given a DOMSource emits some initial vtree', t => {
   t.plan(1)
   const DOMSourceMock = mockDOMSource(xstreamAdapter, {})
-  const { DOM } = main({ DOM: DOMSourceMock })
-  DOM.addListener({next: vtree => {
+  const { DOM: vtree$ } = main({ DOM: DOMSourceMock })
+  vtree$.addListener({next: vtree => {
     // if there’s an official way to test
     // whether an object is a vnode
     // we should use that, instead of this.
