0817b8a43cc4d3ef59303e445ed6da99857e7b86
Shahar Or <mightyiampresence@gmail.com>
2016-11-18 23:38:08 +0200

    mockPathWithSpyThatReturnsSymbol discovers arity

--- utils/mock-path-with-spy-that-returns-symbol.js
+++ utils/mock-path-with-spy-that-returns-symbol.js
@@ -3,13 +3,14 @@ const { spy: spyOn } = require('simple-spy')
 const path = require('path')
 const R = require('ramda')
 
-const mockPathWithSpyThatReturnsSymbol = (originalDirname, originalRelativePath, arity = 0) => {
+const mockPathWithSpyThatReturnsSymbol = (originalDirname, originalRelativePath) => {
   const symbol = Symbol(originalRelativePath)
   const stub = () => symbol
-  Object.defineProperty(stub, 'length', { value: arity })
-  const spy = spyOn(stub)
   const absolutePath = path.resolve(originalDirname, originalRelativePath)
   const relativePath = path.relative(__dirname, absolutePath)
+  const arity = require(relativePath).length
+  Object.defineProperty(stub, 'length', { value: arity })
+  const spy = spyOn(stub)
   mock(relativePath, spy) // impure
   return { symbol, spy }
 }
